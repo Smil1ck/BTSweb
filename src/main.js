81a54ae01483b423de81a4f08aebcc234a16390a
matvey <matvey.errmakov@mail.ru>
2026-01-28 13:38:34 +0300

    vuetify config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { vuetify } from "plugins/vuetify";
 
 import App from "./App.vue";
 import router from "./router";
 
+// Vuetify
+import "vuetify/styles";
 import "@fontsource/roboto/100.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -11,18 +14,7 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "@fontsource/roboto/900.css";
 
-// Vuetify
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-
 const app = createApp(App);
-const vuetify = createVuetify({
-  components,
-  directives,
-  ssr: true,
-});
 
 app.use(createPinia());
 app.use(vuetify);
